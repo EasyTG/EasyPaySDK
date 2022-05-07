f90a6f4fa7746f48e8e074dac949e48183b20192
xiayi <hi@xiayi.li>
2022-05-07 11:04:46 +0800

    add php sdk.

--- php/sdk.php
+++ php/sdk.php
@@ -1,31 +1,44 @@
-<?php
+<?php /** @noinspection DuplicatedCode */
 
 $_easypay_token = "";
 $_easypay_errno = -1;
 $_easypay_error = "";
 
-function easypay_init($appId, $key) {
-    global $_easypay_token = $appId.":".$key;
+function easypay_init($appId, $key)
+{
+    global $_easypay_token;
+    $_easypay_token = $appId . ":" . $key;
 }
 
-function _easypay_url($method, $queryString=null) {
+function _easypay_url($method, $queryString = null)
+{
     global $_easypay_token;
     $extra = "";
     if (isset($queryString)) {
-        $extra = "?".$queryString;
+        $extra = "?" . $queryString;
     }
-	return "https://pay.easypaybot.com/api/merchant/".$_easypay_token."/".$method.$extra;
+    $host = 'pay.easypaybot.com';
+    // getenv("EASYPAY_DEBUG")
+    $debug = getenv("EASYPAY_DEBUG");
+    if ($debug !== false && $debug !== "") {
+        fprintf(STDERR, "WARNING: DEBUG SERVER USED!!!\n");
+        $host = 'pay.test.tgbot.link';
+    }
+    return "https://$host/api/merchant/" . $_easypay_token . "/" . $method . $extra;
 }
 
-function easypay_do() {
+function easypay_error()
+{
+    global $_easypay_errno, $_easypay_error;
+    return sprintf("#%d(%s)", $_easypay_errno, $_easypay_error);
 }
 
-function easypay_order($id) {
+function easypay_me()
+{
+    global $_easypay_errno, $_easypay_error;
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, _easypay_url("order", "order_id=".$id));
-    // curl_setopt($tuCurl, CURLOPT_POST, 1);
-    // curl_setopt($tuCurl, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($tuCurl, CURLOPT_HTTPHEADER, array("User-Agent: EasyPayClient/php1.0"));
+    curl_setopt($ch, CURLOPT_URL, _easypay_url("me"));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: EasyPaySDK/php1.0", "Content-Type: application/json"));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $output = curl_exec($ch);
     if (curl_errno($ch) !== 0) {
@@ -34,7 +47,7 @@ function easypay_order($id) {
         return false;
     }
     curl_close($ch);
-    $resp = json_decode($output);
+    $resp = json_decode($output, true);
     if (!isset($resp['code']) || !isset($resp['status']) || !isset($resp['result']) || $resp['result'] === false) {
         $_easypay_errno = 65535;
         $_easypay_error = "internal error";
@@ -43,13 +56,109 @@ function easypay_order($id) {
     return $resp['result'];
 }
 
-function easypay_transfer($params) {
+function easypay_paylink($params)
+{
+    global $_easypay_errno, $_easypay_error;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, _easypay_url("paylink"));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: EasyPaySDK/php1.0", "Content-Type: application/json"));
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    if (curl_errno($ch) !== 0) {
+        $_easypay_errno = curl_errno($ch);
+        $_easypay_error = curl_error($ch);
+        return false;
+    }
+    curl_close($ch);
+    $resp = json_decode($output, true);
+    if (!isset($resp['code']) || !isset($resp['status']) || !isset($resp['result']['items']) || $resp['result']['items'] === false) {
+        $_easypay_errno = 65535;
+        $_easypay_error = "internal error";
+        return false;
+    }
+    return $resp['result']['items'];
+}
+
+function easypay_trans($type, $txid)
+{
+    global $_easypay_errno, $_easypay_error;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, _easypay_url("trans", "type=${type}&txid=${txid}"));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: EasyPaySDK/php1.0"));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    if (curl_errno($ch) !== 0) {
+        $_easypay_errno = curl_errno($ch);
+        $_easypay_error = curl_error($ch);
+        return false;
+    }
+    curl_close($ch);
+    $resp = json_decode($output, true);
+    if (!isset($resp['code']) || !isset($resp['status']) || !isset($resp['result']) || $resp['result'] === false) {
+        $_easypay_errno = 65535;
+        $_easypay_error = "internal error";
+        return false;
+    }
+    return $resp['result'];
+}
+
+function easypay_order($id)
+{
+    global $_easypay_errno, $_easypay_error;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, _easypay_url("order", "order_id=" . $id));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: EasyPaySDK/php1.0"));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    if (curl_errno($ch) !== 0) {
+        $_easypay_errno = curl_errno($ch);
+        $_easypay_error = curl_error($ch);
+        return false;
+    }
+    curl_close($ch);
+    $resp = json_decode($output, true);
+    if (!isset($resp['code']) || !isset($resp['status']) || !isset($resp['result']) || $resp['result'] === false) {
+        $_easypay_errno = 65535;
+        $_easypay_error = "internal error";
+        return false;
+    }
+    return $resp['result'];
+}
+
+
+function easypay_orderlist($page, $pageSize)
+{
+    global $_easypay_errno, $_easypay_error;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, _easypay_url("orderlist", "page=${page}&page_size=${pageSize}"));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: EasyPaySDK/php1.0"));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    if (curl_errno($ch) !== 0) {
+        $_easypay_errno = curl_errno($ch);
+        $_easypay_error = curl_error($ch);
+        return false;
+    }
+    curl_close($ch);
+    $resp = json_decode($output, true);
+    if (!isset($resp['code']) || !isset($resp['status']) || !isset($resp['result']['items']) || $resp['result']['items'] === false) {
+        $_easypay_errno = 65535;
+        $_easypay_error = "internal error";
+        return false;
+    }
+    return $resp['result']['items'];
+}
+
+function easypay_transfer($params)
+{
+    global $_easypay_errno, $_easypay_error;
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, _easypay_url("transfer"));
-    curl_setopt($tuCurl, CURLOPT_POST, 1);
-    $data = json_encode($params);
-    curl_setopt($tuCurl, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($tuCurl, CURLOPT_HTTPHEADER, array("User-Agent: EasyPayClient/php1.0"));
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: EasyPaySDK/php1.0", "Content-Type: application/json"));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $output = curl_exec($ch);
     if (curl_errno($ch) !== 0) {
@@ -58,7 +167,7 @@ function easypay_transfer($params) {
         return false;
     }
     curl_close($ch);
-    $resp = json_decode($output);
+    $resp = json_decode($output, true);
     if (!isset($resp['code']) || !isset($resp['status']) || !isset($resp['result']) || $resp['result'] === false) {
         $_easypay_errno = 65535;
         $_easypay_error = "internal error";
@@ -67,13 +176,14 @@ function easypay_transfer($params) {
     return $resp['result'];
 }
 
-function easypay_deduct($params) {
+function easypay_deduct($params)
+{
+    global $_easypay_errno, $_easypay_error;
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, _easypay_url("deduct"));
-    curl_setopt($tuCurl, CURLOPT_POST, 1);
-    $data = json_encode($params);
-    curl_setopt($tuCurl, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($tuCurl, CURLOPT_HTTPHEADER, array("User-Agent: EasyPayClient/php1.0"));
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: EasyPaySDK/php1.0", "Content-Type: application/json"));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $output = curl_exec($ch);
     if (curl_errno($ch) !== 0) {
@@ -82,11 +192,101 @@ function easypay_deduct($params) {
         return false;
     }
     curl_close($ch);
-    $resp = json_decode($output);
+    echo $output;
+    $resp = json_decode($output, true);
     if (!isset($resp['code']) || !isset($resp['status']) || !isset($resp['result']) || $resp['result'] === false) {
         $_easypay_errno = 65535;
         $_easypay_error = "internal error";
         return false;
     }
     return $resp['result'];
+}
+
+function easypay_invitecode($code, $tgUserId) {
+    global $_easypay_errno, $_easypay_error;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, _easypay_url("invitecode", "code=${code}&tg_user_id=${tgUserId}"));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: EasyPaySDK/php1.0"));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    if (curl_errno($ch) !== 0) {
+        $_easypay_errno = curl_errno($ch);
+        $_easypay_error = curl_error($ch);
+        return false;
+    }
+    curl_close($ch);
+    $resp = json_decode($output, true);
+    if (!isset($resp['code']) || !isset($resp['status']) || !isset($resp['result']) || $resp['result'] === false) {
+        $_easypay_errno = 65535;
+        $_easypay_error = "internal error";
+        return false;
+    }
+    return $resp['result'];
+}
+
+function easypay_change_commission($commission, $inviteCodeId, $tgUserId) {
+    global $_easypay_errno, $_easypay_error;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, _easypay_url("change_commission", "commission=${commission}&invite_code_id=${inviteCodeId}&tg_user_id=${tgUserId}"));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: EasyPaySDK/php1.0", "Content-Type: application/json"));
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    if (curl_errno($ch) !== 0) {
+        $_easypay_errno = curl_errno($ch);
+        $_easypay_error = curl_error($ch);
+        return false;
+    }
+    curl_close($ch);
+    $resp = json_decode($output, true);
+    if (!isset($resp['code']) || !isset($resp['status']) || !isset($resp['result']) || $resp['result'] === false) {
+        $_easypay_errno = 65535;
+        $_easypay_error = "internal error";
+        return false;
+    }
+    return $resp['result'];
+}
+
+function easypay_userinfo($tgUserId) {
+    global $_easypay_errno, $_easypay_error;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, _easypay_url("userinfo", "tg_user_id=${tgUserId}"));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: EasyPaySDK/php1.0"));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    if (curl_errno($ch) !== 0) {
+        $_easypay_errno = curl_errno($ch);
+        $_easypay_error = curl_error($ch);
+        return false;
+    }
+    curl_close($ch);
+    $resp = json_decode($output, true);
+    if (!isset($resp['code']) || !isset($resp['status']) || !isset($resp['result']) || $resp['result'] === false) {
+        $_easypay_errno = 65535;
+        $_easypay_error = "internal error";
+        return false;
+    }
+    return $resp['result'];
+}
+
+function easypay_fundlogs($page, $pageSize) {
+    global $_easypay_errno, $_easypay_error;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, _easypay_url("fundlogs", "page=${page}&page_size=${pageSize}"));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: EasyPaySDK/php1.0"));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    if (curl_errno($ch) !== 0) {
+        $_easypay_errno = curl_errno($ch);
+        $_easypay_error = curl_error($ch);
+        return false;
+    }
+    curl_close($ch);
+    $resp = json_decode($output, true);
+    if (!isset($resp['code']) || !isset($resp['status']) || !isset($resp['result']['items']) || $resp['result']['items'] === false) {
+        $_easypay_errno = 65535;
+        $_easypay_error = "internal error";
+        return false;
+    }
+    return $resp['result']['items'];
 }
---
+++ php/sdk_test.php
@@ -0,0 +1,50 @@
+<?php
+require(__DIR__ . '/sdk.php');
+
+// easypay_init("626f63fd5d6816e8087e69d3", "aIjkzXPv3z4sj1VM");
+// var_dump(easypay_me());
+/*var_dump(easypay_paylink([
+    'items' => [
+        [
+            'unique_id' => '1',
+            'name' => 'item1',
+            'amount' => 100,
+            'auto_renew' => true,
+        ],
+        [
+            'unique_id' => '2',
+            'name' => 'item2',
+            'amount' => 200,
+            'auto_renew' => false,
+        ],
+        [
+            'unique_id' => '3',
+            'name' => 'item3',
+            'amount' => 500,
+            'auto_renew' => false,
+        ],
+    ],
+    'params' => 'a=b&c=d',
+    'return_url' => 'https://t.me/SuperIndexBot?start=xxxxxxx',
+    'expired_at' => time()+86400,
+]), easypay_error());*/
+// var_dump(easypay_trans("tron", "12a9100a92a4a4efca1a6f173eb77898a36d15a8df7093e696d175b7dff522a9"));
+// var_dump(easypay_order('6275280114fdacffad851af4'));
+// var_dump(easypay_orderlist(1, 1000));
+/*var_dump(easypay_transfer([
+    'order_id' => ''.time(),
+    'name' => '转账',
+    'amount' => 200,
+    'to_user_id' => 928345358,
+]));*/
+/*var_dump(easypay_deduct([
+    'order_id' => '6275280114fdacffad851af4',
+    'unique_id' => '6275280114fdacffad851af4',
+    'name' => '转账',
+    'amount' => 200,
+    'use_promo' => true,
+]));*/
+// var_dump(easypay_invitecode('ejbu6nfn', '928345358'));
+// var_dump(easypay_change_commission(10, '626f881cf0b6d7625ac9c23b', '812342452'));
+// var_dump(easypay_userinfo('928345358'));
+// var_dump(easypay_fundlogs(1, 1000));
